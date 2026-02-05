8a0f4820fd985e3c6d989ae1cf52829d95f78cd4
Mr Noor <mrnoorin@gmail.com>
2026-02-05 21:36:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="noor-cli",                 # PyPI package name
-    version="1.0.2",
+    name="noor-cli",                 # PyPI name (unique)
+    version="1.0.4",                 # change for every release
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "noor=noor.cli:main"
+            "noor=noor.cli:main"     # user runs: noor
         ]
     },
     author="Noor",
-    description="Live age calculator showing your life in seconds",
+    description="Live age calculator showing life in seconds",
     python_requires=">=3.8",
 )
