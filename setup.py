b08d94f75fb158f0775fac7d83c91b61ed7afbd8
Mr Noor <mrnoorin@gmail.com>
2026-02-05 18:22:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="noor",                 # PyPI package name
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
